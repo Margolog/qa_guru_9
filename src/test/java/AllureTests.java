ccd8b8f2e97f24e0fa685a9e50866e5f494c36f9
Margarita Logunova <logunovar@gmail.com>
2026-05-16 18:50:51 +0300

    first

---
+++ src/test/java/AllureTests.java
@@ -0,0 +1,68 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class AllureTests {
+
+    @Test
+    @DisplayName("Чистый Selenide (с Listener)")
+    public void testIssueSearchWithListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $("[data-target='qbsearch-input.inputButton']").click();
+        $("#query-builder-test").sendKeys("Margolog/qa_guru_9");
+        $("#query-builder-test").pressEnter();
+
+        $(linkText("Margolog/qa_guru_9")).click();
+        $("#issues-tab").click();
+        $(withText("Issues")).should(Condition.exist);
+    }
+
+    @Test
+    @DisplayName("Лямбда шаги через step (name, () -> {})")
+    public void testIssueSearchWithSteps() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+
+        step("Ищем репу", () -> {
+            $("[data-target='qbsearch-input.inputButton']").click();
+            $("#query-builder-test").sendKeys("Margolog/qa_guru_9");
+            $("#query-builder-test").pressEnter();
+        });
+
+        step("Кликаем на нужный репозиторий", () -> {
+            $(linkText("Margolog/qa_guru_9")).click();
+            $("#issues-tab").click();
+        });
+
+        step("Проверяем наличие Issues", () -> {
+            $(withText("Issues")).should(Condition.exist);
+        });
+    }
+
+    @Test
+    @DisplayName("Шаги с аннотацией @Step")
+    public void testIssueSearchWithWebSteps() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository("Margolog/qa_guru_9");
+        steps.clickOnRepositoryLink("Margolog/qa_guru_9");
+        steps.shouldSeeIssue();
+        steps.takeScreenshot();
+    }
+}
--- src/test/java/WebSteps.java
+++ src/test/java/WebSteps.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
 
 public class WebSteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репу")
+    public void searchForRepository(String repo) {
+        $("[data-target='qbsearch-input.inputButton']").click();
+        $("#query-builder-test").sendKeys("Margolog/qa_guru_9");
+        $("#query-builder-test").pressEnter();
+    }
+
+    @Step("Кликаем на нужный репозиторий")
+    public void clickOnRepositoryLink(String repo) {
+        $(linkText("Margolog/qa_guru_9")).click();
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверяем наличие Issues")
+    public void shouldSeeIssue() {
+        $(withText("Issues")).should(Condition.exist);
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
 }
