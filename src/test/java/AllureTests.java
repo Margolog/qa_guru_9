7bc6f7b2c5f05c14479102eb7de1bb94d694f1d6
Margarita Logunova <logunovar@gmail.com>
2026-05-17 18:44:53 +0300

    first

--- src/test/java/AllureTests.java
+++ src/test/java/AllureTests.java
@@ -17,18 +17,10 @@ public class AllureTests {
 
     @BeforeAll
     static void setup() {
-        Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.headless = true;
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-
-        Configuration.browserCapabilities = options;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
